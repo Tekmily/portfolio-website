c2deec6b82a72e0564676aaf6c291bab592eeff8
devtagcabuang <trishiaann.academics@gmail.com>
2022-08-18 22:24:42 +0800

    Added certifications list

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -9,10 +9,13 @@
                 <h1 class="global-head1">ABOUT HER</h1>
                 <p>She studied Bachelor of Science in Computer Science at the Polytechnic University of the Philippines. She has worked as Content Management System (CMS) Developer & Designer, and has also worked as SEO Specialist since 2018. She excels at designing and developing websites with HTML and CSS.</p>
             </section>
-
-            <section id="certification-section">
-                <CertificationsSection />
-            </section>
+            
+<!-- CERTIFICATIONS LIST -->
+            <div id="certifications">
+                <h3>CERTIFICATIONS</h3>
+                <img :src="require('@/assets/web-design-certificate.png')">
+                <p><a href="https://freecodecamp.org/certification/fccfefd5801-2c85-4717-a21a-95e74c7cdea5/responsive-web-design" target="_blank">Responsive Web Design</a></p>
+            </div>
         </div>
 
         <div id="right-side-about">
@@ -21,9 +24,6 @@
     </div>
 </template>
 
-<script>
-import CertificationsSection from './items/CertificationsSection.vue'
-</script>
 
 <style scoped>
 .flex-about {
