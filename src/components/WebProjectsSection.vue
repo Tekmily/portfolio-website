39a31ea5a82323f7bf9e53222f9925a5caa975f6
devtagcabuang <trishiaann.academics@gmail.com>
2022-08-19 21:51:35 +0800

    Added image file

--- src/components/WebProjectsSection.vue
+++ src/components/WebProjectsSection.vue
@@ -18,28 +18,28 @@ import WebProjectsItem from './items/WebProjectsItem.vue'
 const contents = [
     {
         id: 0,
-        image: ,
+        image: "atlas.png",
         title: "ATLAS DOOR REPAIR",
         a: 'https://atlasdoorrepair.com/',
         body: "WordPress was used to create and maintain this website. I worked on this client website for four years. Aside from web maintenance, I also did SEO for Atlas, and the majority of the target keywords ranked first to third in Google. I also worked on Atlas' Landing Pages for Marketing using Click Funnels."
     }, //END OF ATLAS
     {
         id: 1,
-        image: ,
+        image: "naperville.png",
         title: "NAPERVILLE SERVICE LIMO",
         a: 'https://www.napervilleservicelimo.com/',
         body: "Weebly was used to create and maintain this website. I worked on this client website for four years. Aside from web maintenance, I also did SEO for Naperville, and the majority of the target keywords ranked first to second in Google."
     }, //END OF NAPERVILLE
     {
         id: 3,
-        image: ,
+        image: "bitc.png",
         title: "BITC ROOF TILES",
         a: 'https://www.bitcrooftiles.com/',
         body: "WordPress was used to create and maintain this website. I worked on this client website for two years. Aside from web maintenance, I also did SEO for BITC, and the majority of the target keywords ranked first to third in Google."
     }, //END OF BITC
     {
         id: 4,
-        image: ,
+        image: "cardetail.png",
         title: "MY CAR DETAIL CHICAGO",
         a: 'https://www.mycardetailchicago.com/',
         body: "WordPress was used to create and maintain this website. I worked on this client website for four years. Aside from web maintenance, I also did SEO for Naperville, and the majority of the target keywords ranked third to fifth in Google."
