f19e5cf9b7acf9de79471ff0e24aa7e5cc7c8f9e
devtagcabuang <trishiaann.academics@gmail.com>
2022-08-24 21:25:06 +0800

    Update resumer version 4

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -14,7 +14,7 @@
             </h1>
             <p class="sub-text">SHE ASPIRES TO CRAFT WEB EXPERIENCES THAT TYPIFY AND INSPIRE.</p>
             <div class="resume">
-                <a href="https://drive.google.com/file/d/15gDiNbYYOxvRjRyvtvLmRt32Wzs3f1si/view?usp=sharing" target="_blank">DOWNLOAD RESUME</a>
+                <a href="https://drive.google.com/file/d/1IcY7AjrpxTsDeuLBso6Er_L8sqvR6vap/view?usp=sharing" target="_blank">DOWNLOAD RESUME</a>
             </div>
         </div>
     </div>
